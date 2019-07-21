fafac78c2aa98c0de055306285115a36bc2a549d
Patrick <hansenp2@tcnj.edu>
2019-07-20 20:30:54 -0400

    api requests from front end

--- client/src/components/TodoList.js
+++ client/src/components/TodoList.js
@@ -1,34 +1,84 @@
 import React from 'react';
 
+import TodoItem from './TodoItem'
+
 class TodoList extends React.Component {
 
   state = {
-    data: []
+    data: [],
+    text: ''
   }
 
   componentDidMount = async () => {
     const resp = await fetch('http://localhost:8000/api/todo-list');
     const data = await resp.json();
-    console.log({data, });
+    console.log({resp, });
     this.setState({ data: data.data })
+  }
+
+  inputChangeHandler = (event) => {
+    const currentString = event.target.value;
+    const currentChars = currentString.split('');
+
+    this.setState({
+      text: currentString,
+      numChars: currentString.length,
+      allChars: currentChars
+     })
+  }
+
+  buttonClickHandler = (event) => {
+    const { data, text } = this.state;
+
+    const newItem = {
+      'title': text,
+      'description': 'blank',
+      'creation_time': 'blank',
+      'completed': false
+    }
+
+    data.push(newItem);
+    this.setState({ data: data }, () => {
+      console.log('[POST]', data);
+      this.postData('http://localhost:8000/api/todo-list', {'data': data});
+    });
+  }
+
+  postData = async (url, data) => {
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(data)
+    });
+    console.log('[POSTED]', response);
   }
 
   render() {
 
     const { data } = this.state;
-    console.log(data.length);
-    console.log(data);
-
-    const test = data[0];
-    console.log(test);
 
     return (
       <div>
 
+        <div>
+          <span>
+            <input
+              type="text"
+              onChange={this.inputChangeHandler}
+              value={this.state.text} />
+          </span>
+          <span>
+            <button onClick={this.buttonClickHandler}>Add</button>
+          </span>
+        </div>
+
         {data.map((item, index) => {
           return (
             <div key={index}>
-              <p>{item.title}</p>
+              <TodoItem
+                title={item.title}
+                description={item.description}
+                creationTime={item.creation_time}
+                completed={item.completed}/>
             </div>
           )
         })}
