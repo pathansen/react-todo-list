b0ffc88b99a3d115172e556c8aea11558f662819
Patrick <hansenp2@tcnj.edu>
2019-07-20 20:29:54 -0400

    get and post requests on back end

--- server/app/todo_list.py
+++ server/app/todo_list.py
@@ -1,3 +1,4 @@
+import os
 import json
 import falcon
 
@@ -5,25 +6,31 @@ import falcon
 class TodoList(object):
 
     def on_get(self, req, resp):
-        data = {
-            'data': [
-                {
-                    'title': 'Item 1',
-                    'description': 'Description 1',
-                    'creation_time': 'time 1',
-                    'completed': False
-                },
-                {
-                    'title': 'Item 2',
-                    'description': 'Description 2',
-                    'creation_time': 'time 2',
-                    'completed': False
-                }
-            ]
-        }
+        """
+        Get data from server to send to client.
+        """
+
+        # Open JSON file holding todo list items
+        data_file = os.path.join(os.getcwd(), 'data', 'data.json')
+        if os.path.isfile(data_file):
+            with open(data_file, 'r') as f:
+                data = json.load(f)
+        else:  # data file doesn't exist
+            data = {'data': []}
 
         resp.body = json.dumps(data, ensure_ascii=True)
         resp.status = falcon.HTTP_200
 
     def on_post(self, req, resp):
-        pass
+        """
+        Save data from client to server.
+        """
+
+        # Decode response from string to JSON
+        data = json.loads(req.stream.read())
+
+        # Open JSON file holding todo list items
+        with open(os.path.join(os.getcwd(), 'data/data.json'), 'w') as f:
+            json.dump(data, f, indent=2)
+
+        resp.status = falcon.HTTP_201
